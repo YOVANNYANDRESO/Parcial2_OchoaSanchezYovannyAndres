1c093cc36f78f6f2dabb5ab47777d6ce253d2f9d
YOVANNYOCHOA <yovannyandresochoa@gmail.com>
2023-04-02 08:21:03 -0500

    continued en the seeder

--- ConcertDB/ConcertDB/DAL/SeederDb.cs
+++ ConcertDB/ConcertDB/DAL/SeederDb.cs
@@ -1,4 +1,6 @@
-﻿namespace ConcertDB.DAL
+﻿using ConcertDB.DAL.Entities;
+
+namespace ConcertDB.DAL
 {
     public class SeederDb
     {
@@ -9,6 +11,18 @@
             _context = context;
         }
 
+        public  async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();//remplaza el update-database
+            await PopulateTicketssAsync();
+        }
 
+        private async Task PopulateTicketssAsync()
+        {
+            if(!_context.Ticketss.Any())
+            {
+                _context.Ticketss.Add(new Ticket { });
+            }
+        }
     }
 }
